e57b479cbd1dfc59e49b81bf67e7a6107c0b6241
Jackson Rodrigues <jacksonnalmeida30@gmail.com>
2026-02-02 08:14:17 -0400

    feature: add combiners count

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import apache_beam as beam
+from typing import Any
 from apache_beam import Pipeline
 from apache_beam.runners.runner import PipelineResult
 
@@ -8,38 +9,41 @@ def findWords(word):
 def create_pipeline() -> PipelineResult:
     pipe: Pipeline = beam.Pipeline()
 
-    model_cars: tuple[str, ...] = ("Gol", "Fiesta", "Civic", "Corolla", "Onix", "HB20", "Uno", "Sandero", "Prisma",
-                                     "Argo")
-
-    model_motorcycle: tuple[str, ...] = ("CG 160", "XRE 300", "Ninja 400", "CB 500", "Hornet 160R", "MT-07", "CB 1000R",
-                                    "Ténéré 700", "R1", "Africa Twin")
-
-    cars_pc = (
-        pipe
-        | "criando pcollection cars" >> beam.Create(model_cars)
-    )
-
-    motorcycles_pc = (
-        pipe
-        | "criando pcollection motorcycles" >> beam.Create(model_motorcycle)
-    )
-
-    veiches_pc = (
-    (cars_pc, motorcycles_pc) | beam.Flatten()
-    )
-
-    veiches_pc | beam.Map(print)
-
-    veiches_pc | beam.io.WriteToText("./veiches.txt")
-
-    # pCollection: Any = (
-    #         pipe
-    #         | "importar dados" >> beam.io.ReadFromText("./voos_sample.csv", skip_header_lines=1)
-    #         | "separar por virgulas" >> beam.Map(lambda record: record.split(","))
-    #         | "Filtrar os dados por Los Angeles" >> beam.Filter(lambda record: record[3] == "LAX")
-    #         # | "Mostrar resultado" >> beam.Map(print)
-    #         | "Gravar resultados" >> beam.io.WriteToText("./voos.txt")
+    # model_cars: tuple[str, ...] = ("Gol", "Fiesta", "Civic", "Corolla", "Onix", "HB20", "Uno", "Sandero", "Prisma",
+    #                                  "Argo")
+    #
+    # model_motorcycle: tuple[str, ...] = ("CG 160", "XRE 300", "Ninja 400", "CB 500", "Hornet 160R", "MT-07", "CB 1000R",
+    #                                 "Ténéré 700", "R1", "Africa Twin")
+    #
+    # cars_pc = (
+    #     pipe
+    #     | "criando pcollection cars" >> beam.Create(model_cars)
     # )
+    #
+    # motorcycles_pc = (
+    #     pipe
+    #     | "criando pcollection motorcycles" >> beam.Create(model_motorcycle)
+    # )
+    #
+    # veiches_pc = (
+    # (cars_pc, motorcycles_pc) | beam.Flatten()
+    # )
+    #
+    # veiches_pc | beam.Map(print)
+    #
+    # veiches_pc | beam.io.WriteToText("./veiches.txt")
+
+    pCollection: Any = (
+            pipe
+            | "importar dados" >> beam.io.ReadFromText("./voos_sample.csv", skip_header_lines=1)
+            | "separar por virgulas" >> beam.Map(lambda record: record.split(","))
+            # | "Filtrar os dados por Los Angeles" >> beam.Filter(lambda record: record[9] == "LAX")
+            | "pegar voo" >> beam.Filter(lambda record: int(record[8]) > 0)
+            | "criar par" >> beam.Map(lambda record: (record[4], int(record[8])))
+            | "contar por keys" >> beam.combiners.Count.PerKey()
+            | "Mostrar resultado" >> beam.Map(print)
+            # | "Gravar resultados" >> beam.io.WriteToText("./voos.txt")
+    )
 
     # pCollectionPoema: Any = (
     #     pipe
